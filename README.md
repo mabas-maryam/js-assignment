1b8783fa30d501e1a537f49b7e171813b13887cc
mabas-maryam <mabas.maryamidimabas@gmail.com>
2019-10-04 10:00:48 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # js-assignment
+# Name: Mabas Maryam Idi
+
+## Question: Find a way to maintain the header when you add an additional information to the table.
---
+++ app.js
@@ -0,0 +1,352 @@
+var ITStudents = [
+  {
+    name: "Mabas Maryam Idi",
+    age: 20,
+    address: "Zaria Road, Jos.",
+    tribe: "Challa",
+    department: "Computer Science",
+    level: 400,
+    gender: "Female",
+    height: 1.6,
+    grade: [80, 100, 70, 85, 98]
+  }
+  //   {
+  //     name: "Oyekale Oluwatosin Alice",
+  //     age: 23,
+  //     address: "Farin Gada",
+  //     tribe: "Yoruba",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.6,
+  //     grade: [80, 60, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Bok Victoria Dung",
+  //     age: 20,
+  //     address: "Forestry",
+  //     tribe: "Berom",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.58,
+  //     grade: [80, 65, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Dauda Yohanna Awari",
+  //     age: 22,
+  //     address: "Anguwan Rukuba",
+  //     tribe: "Afizere",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.76,
+  //     grade: [80, 60, 71, 85, 98]
+  //   },
+  //   {
+  //     name: "Emmanuel Elizabeth John",
+  //     age: 25,
+  //     address: "Farin Gada",
+  //     tribe: "Ibilio",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.76,
+  //     grade: [80, 60, 70, 80, 98]
+  //   },
+  //   {
+  //     name: "Bamigboye Christiana Temitope",
+  //     age: 26,
+  //     address: "Nassarawa Gwom",
+  //     tribe: "Yoruba",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.76,
+  //     grade: [80, 60, 70, 85, 90]
+  //   },
+  //   {
+  //     name: "Ibedu Ifeoma Franca",
+  //     age: 25,
+  //     address: "Dogon Dutse",
+  //     tribe: "Igbo",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.6,
+  //     grade: [80, 60, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Job Gift Eleojo",
+  //     age: 23,
+  //     address: "Three container",
+  //     tribe: "Igala",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.6,
+  //     grade: [80, 60, 65, 85, 98]
+  //   },
+  //   {
+  //     name: "Adams Mark Oche",
+  //     age: 16,
+  //     address: "jos",
+  //     tribe: "Idoma",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.6,
+  //     grade: [80, 60, 50, 85, 98]
+  //   },
+  //   {
+  //     name: "Stephen Nanret F",
+  //     age: 23,
+  //     address: "Ring Road",
+  //     tribe: "Mwagavul",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.5,
+  //     grade: [80, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Kaslong Kevin S",
+  //     age: 23,
+  //     address: "Tundu wada",
+  //     tribe: "Goemai",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.75,
+  //     grade: [35, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Yada Martins Tisan",
+  //     age: 25,
+  //     address: "Odus",
+  //     tribe: "Kagoro",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.58,
+  //     grade: [80, 45, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Susan Owoicho",
+  //     age: 30,
+  //     address: "Farin gada",
+  //     tribe: "Idoma",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.65,
+  //     grade: [80, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Josphine Sunday",
+  //     age: 25,
+  //     address: "Odus",
+  //     tribe: "Eggon",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.56,
+  //     grade: [80, 40, 60, 85, 98]
+  //   },
+  //   {
+  //     name: "Asalu Temitope Peter",
+  //     age: 23,
+  //     address: "Ring Road",
+  //     tribe: "Yoruba",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.69,
+  //     grade: [80, 70, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Uzaifat Ibrahim",
+  //     age: 24,
+  //     address: "Dodo Street",
+  //     tribe: "Mwagavul",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.5,
+  //     grade: [70, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Ohue Joy Osedebamen",
+  //     age: 16,
+  //     address: "Ring Road",
+  //     tribe: "Esan",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.5,
+  //     grade: [80, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Nanbal Moses Kumdam",
+  //     age: 23,
+  //     address: "Odus",
+  //     tribe: "Mupun",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.5,
+  //     grade: [80, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Musa Deborah kibba ",
+  //     age: 23,
+  //     address: "Odus",
+  //     tribe: "Berom",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Female",
+  //     height: 1.5,
+  //     grade: [80, 40, 70, 85, 98]
+  //   },
+  //   {
+  //     name: "Ponfah Micheal",
+  //     age: 22,
+  //     address: "Odus",
+  //     tribe: "Birom",
+  //     department: "Computer Science",
+  //     level: 300,
+  //     gender: "Male",
+  //     height: 1.5,
+  //     Grade: [80, 40, 70, 85, 98]
+  //   }
+];
+var table = document.getElementById("table");
+var Thread = () => {
+  var thead = document.createElement("thead");
+
+  var th1 = document.createElement("th");
+  th1.innerHTML = "Name";
+
+  var th2 = document.createElement("th");
+  th2.innerHTML = "Age";
+
+  var th3 = document.createElement("th");
+  th3.innerHTML = "Address";
+  var th4 = document.createElement("th");
+  th4.innerHTML = "Tribe";
+
+  var th5 = document.createElement("th");
+  th5.innerHTML = "Department";
+
+  var th6 = document.createElement("th");
+  th6.innerHTML = "Level";
+
+  var th7 = document.createElement("th");
+  th7.innerHTML = "Gender";
+
+  var th8 = document.createElement("th");
+  th8.innerHTML = "Height";
+
+  var th8 = document.createElement("th");
+  th8.innerHTML = "Grade";
+  thead.append(th1, th2, th3, th4, th5, th6, th7, th8);
+  table.appendChild(thead);
+};
+
+var creatTable = () => {
+  Thread();
+
+  ITStudents.forEach(student => {
+    var tr = document.createElement("tr");
+    tr.style.textAlign = "center";
+    tr.style.backgroundColor = "LightCoral";
+
+    var td1 = document.createElement("td");
+    td1.innerHTML = student.name;
+    td1.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td2 = document.createElement("td");
+    td2.innerHTML = student.age;
+    td2.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td3 = document.createElement("td");
+    td3.innerHTML = student.address;
+    td3.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td4 = document.createElement("td");
+    td4.innerHTML = student.tribe;
+    td4.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td5 = document.createElement("td");
+    td5.innerHTML = student.department;
+    td5.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td6 = document.createElement("td");
+    td6.innerHTML = student.level;
+    td6.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td7 = document.createElement("td");
+    td7.innerHTML = student.gender;
+    td7.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td8 = document.createElement("td");
+    td8.innerHTML = student.height;
+    td8.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    var td9 = document.createElement("td");
+    td9.innerHTML = student.grade;
+    td9.style.textAlign = "center";
+    td1.style.backgroundColor = "LightCoral";
+
+    tr.append(td1, td2, td3, td4, td5, td6, td7, td8, td9);
+    table.appendChild(tr);
+  });
+};
+creatTable();
+var Name = document.getElementById("name");
+var Age = document.getElementById("age");
+var Address = document.getElementById("address");
+var Tribe = document.getElementById("tribe");
+var Department = document.getElementById("department");
+var Level = document.getElementById("level");
+var Gender = document.getElementById("gender");
+var Height = document.getElementById("height");
+var Grade = document.getElementById("grade");
+
+document.getElementById("btn").addEventListener("click", () => {
+  table.innerHTML = "";
+  var student = {
+    name: Name.value,
+    age: Age.value,
+    address: Address.value,
+    tribe: Tribe.value,
+    department: Department.value,
+    level: Level.value,
+    gender: Gender.value,
+    height: Height.value,
+    grade: Grade.value
+  };
+  ITStudents.push(student);
+  console.log(ITStudents);
+  creatTable();
+
+  /*console.log(
+    Name.value,
+    age.value,
+    address.value,
+    tribe.value,
+    department.value,
+    level.value,
+    gender.value,
+    height.value,
+    grade.value*/
+  //);
+});
